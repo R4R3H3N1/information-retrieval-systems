7b7aca7447db3ce09464539e5e6ff1a915fec7f3
Max Ibbels <maxibbels55@gmail.com>
2022-06-20 20:08:29 +0200

    add query interface

--- configuration.py
+++ configuration.py
@@ -11,7 +11,7 @@ TERM_SPLIT_CHARACTERS = ["\" ", " \"", "?", ", ", "!", ".\n", ". ", "&", "\n", "
 ABSTRACT_BEGINNINGS = ["Abstract", "Preface", "Summary", "Short", "Synopsis", "Excerpt"]
 
 # Amount of documents that should be returned
-TOP_K_DOCS = 20
+TOP_K_DOCS = 10
 
 # K inside wf_t_d method
 K = 1.2
@@ -25,3 +25,8 @@ CAlC_elevenAP = True
 
 USE_EVAL_QUERIES = True
 EVAL_QUERIES = ['PLAIN-121', 'PLAIN-1021', 'PLAIN-15', 'PLAIN-145', 'PLAIN-1336']
+
+# for 11 AP
+#EVAL_QUERIES = ['PLAIN-121', 'PLAIN-1021', 'PLAIN-15', 'PLAIN-145', 'PLAIN-1336', 'PLAIN-3', 'PLAIN-4', 'PLAIN-5', 'PLAIN-6','PLAIN-7', 'PLAIN-8', 'PLAIN-9',
+#                'PLAIN-10', 'PLAIN-1016', 'PLAIN-2093', 'PLAIN-2094', 'PLAIN-2107', 'PLAIN-2108','PLAIN-2114','PLAIN-2115','PLAIN-2116','PLAIN-2117' ]
+
--- run.py
+++ run.py
@@ -6,12 +6,28 @@ import configuration
 import retrieval_metrics
 import LSI.retrieval_lsi
 
+
+def query(indexer):
+    while True:
+        query_string = input("Enter query: ")
+        k = input("Enter top k: ")
+        if query_string == "exit()":
+            break
+        print("--------------------------------------------------")
+        start = time.time()
+        result = indexer.retrieve_k(query_string, int(k))
+        print(f'Found {len(result)} documents in {round(time.time() - start, 3)} seconds:')
+        print(result)
+        print("--------------------------------------------------")
+
+
 if __name__ == '__main__':
 
     # Create and Evaluate Vector Space Model
     i_vector_space = retrieval.VectorSpaceModel(os.path.join(os.getcwd(), "data", configuration.DOCS_FILE))
-    scorer = retrieval_metrics.RetrievalScorer(i_vector_space)
-    scorer.eval()
+    query(i_vector_space)
+    #scorer = retrieval_metrics.RetrievalScorer(i_vector_space)
+    #scorer.eval()
 
     # # Create LSI model
     # i_lsi = LSI.retrieval_lsi.LatentSemanticIndex(os.path.join(os.getcwd(), "data", configuration.DOCS_FILE))
